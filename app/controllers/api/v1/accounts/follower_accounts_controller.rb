268cfdc58f93a3835b8e1642f7ec781dc24b5d69
atsu1125 <atsuchan@atsuchan.page>
2022-06-29 00:35:47 +0900

    Hide blocked

--- app/controllers/api/v1/accounts/follower_accounts_controller.rb
+++ app/controllers/api/v1/accounts/follower_accounts_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::Accounts::FollowerAccountsController < Api::BaseController
   end
 
   def load_accounts
-    return [] if hide_results?
 
     scope = default_accounts
     scope = scope.where.not(id: current_account.excluded_from_timeline_account_ids) unless current_account.nil? || current_account.id == @account.id
