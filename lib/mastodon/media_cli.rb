81adde7d7850a60f2b26f94bfb54531e54c54911
Claire <claire.github-309c@sitedethib.com>
2024-05-15 15:11:13 +0200

    Fix off-by-one in `tootctl media` commands (#30306)

--- lib/mastodon/media_cli.rb
+++ lib/mastodon/media_cli.rb
@@ -100,7 +100,7 @@ module Mastodon
 
             model_name      = path_segments.first.classify
             attachment_name = path_segments[1].singularize
-            record_id       = path_segments[2..-2].join.to_i
+            record_id       = path_segments[2...-2].join.to_i
             file_name       = path_segments.last
             record          = record_map.dig(model_name, record_id)
             attachment      = record&.public_send(attachment_name)
@@ -143,7 +143,7 @@ module Mastodon
           end
 
           model_name      = path_segments.first.classify
-          record_id       = path_segments[2..-2].join.to_i
+          record_id       = path_segments[2...-2].join.to_i
           attachment_name = path_segments[1].singularize
           file_name       = path_segments.last
 
@@ -269,7 +269,7 @@ module Mastodon
       end
 
       model_name = path_segments.first.classify
-      record_id  = path_segments[2..-2].join.to_i
+      record_id  = path_segments[2...-2].join.to_i
 
       unless PRELOAD_MODEL_WHITELIST.include?(model_name)
         say("Cannot find corresponding model: #{model_name}", :red)
@@ -319,7 +319,7 @@ module Mastodon
         next unless [7, 10].include?(segments.size)
 
         model_name = segments.first.classify
-        record_id  = segments[2..-2].join.to_i
+        record_id  = segments[2...-2].join.to_i
 
         next unless PRELOAD_MODEL_WHITELIST.include?(model_name)
 
